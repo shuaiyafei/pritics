c79dd4dd8f15b66cb48b11966bec51957993b136
shuaiyafei <shuaiyafei@haodf.com>
2018-11-27 10:14:54 +0800

    u

---
+++ webpack/README.md
@@ -0,0 +1,14 @@
+### webpack
+
+#### 加载css
+
+```
+css-loader  file-loader
+```
+
+#### 加载背景和图片
+
+```
+file-loader
+```
+
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const component = () => {
     const element = document.createElement('div');
     element.innerHTML = _.join(['hello ', 'world'], '');
+    element.classList.add('desc');
     return element;
 }
 

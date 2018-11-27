662a2eb379e0ba78b65ecf60ae38da47f0ad0a03
shuaiyafei <shuaiyafei@haodf.com>
2018-11-27 12:05:44 +0800

    u

--- webpack/README.md
+++ webpack/README.md
@@ -3,7 +3,8 @@
 #### 加载css
 
 ```
-css-loader  file-loader
+css-loader
+file-loader
 ```
 
 #### 加载css处理器 (sass less stylus)
@@ -17,19 +18,36 @@ stylus-loader
 #### 加载背景和图片
 
 ```
-file-loader  url-loader image-webpack-loader
+file-loader
+url-loader
+image-webpack-loader
 ```
 
 #### 加载字体
 
 ```
-file-loader url-loader
+file-loader
+url-loader
 ```
 
 #### 加载数据(CSV、TSV、XML)
 
 ```
-
+csv-loader
+xml-loader
 ```
 
+#### 生态生成bundle
 
+```
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        app: './src/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist');
+    }
+}
+```
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,30 +1,46 @@
 import _ from 'lodash';
-import './style.css';
-import Image from './image.jpg';
-import Data from './data.xml';
-
-console.log(Data);
+import printMe from './print';
+// import './style.css';
+// import Image from './image.jpg';
+// import Data from './data.xml';
 
 const component = () => {
-    // 根节点
-    const  root = document.createElement('div');
+    // root
+    const root = document.createElement('div');
 
     const element = document.createElement('div');
     element.innerHTML = _.join(['hello ', 'world'], '');
     element.classList.add('desc');
-    root.append(element);
+    root.appendChild(element);
 
-    const img = document.createElement('img');
-    img.classList.add('show');
-    img.src = Image;
-    root.appendChild(img);
+    const btn = document.createElement('button');
+    btn.innerHTML = 'click me';
+    btn.onclick = printMe;
+    root.appendChild(btn);
     
-    const text = document.createElement('p');
-    text.innerHTML = JSON.stringify(Data);
-    root.appendChild(text);
-
     return root;
 }
+
+// const component = () => {
+//     // 根节点
+//     const  root = document.createElement('div');
+
+//     const element = document.createElement('div');
+//     element.innerHTML = _.join(['hello ', 'world'], '');
+//     element.classList.add('desc');
+//     root.append(element);
+
+//     const img = document.createElement('img');
+//     img.classList.add('show');
+//     img.src = Image;
+//     root.appendChild(img);
+    
+//     const text = document.createElement('p');
+//     text.innerHTML = JSON.stringify(Data);
+//     root.appendChild(text);
+
+//     return root;
+// }
 
 document.body.appendChild(component());
 
